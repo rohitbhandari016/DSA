bd2d6f9dd52043fc3e82361db1121de997ecfb3a
rohitbhandari016 <rohitbhandariaz@gmail.com>
2023-06-28 21:31:11 +0530

    Tree

--- Tree/tree.js
+++ Tree/tree.js
@@ -1,18 +1,31 @@
-class Node{
-    constructor(data){
-        this.data = data;
-        this.left = null;
-        this.right = null;
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
     }
-}
-
-const root = new Node(1);
-
-const node1 = new Node(2);
-
-const node2 = new Node(4);
-
-root.left = node1;
-root.right = node2;
-
-console.log(root);
+  }
+  
+  class BinaryTree {
+    constructor() {
+      this.i = -1;
+    }
+  
+    addNode(list) {
+      this.i++;
+      if (list[this.i] === -1) {
+        return null;
+      }
+      const node = new Node(list[this.i]);
+      node.left = this.addNode(list);
+      node.right = this.addNode(list);
+      return node;
+    }
+  }
+  
+  const preorder = [1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1];
+  
+  const t = new BinaryTree();
+  const root = t.addNode(preorder);
+  console.log(root);
+  
