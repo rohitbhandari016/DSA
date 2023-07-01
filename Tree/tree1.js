5dec1cf772dadd5277725c549a64568b8c254408
rohitbhandari016 <rohitbhandariaz@gmail.com>
2023-07-01 21:24:40 +0530

    Tree: Revision

---
+++ Tree/tree1.js
@@ -0,0 +1,33 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree{
+    constructor(){
+        this.i = -1;
+    }
+
+    add(preorder){
+        this.i++;
+
+        if(preorder[this.i] === -1){
+            return null
+        }
+        const node = new Node(preorder[this.i]);
+        node.left = this.add(preorder);
+        node.right = this.add(preorder);
+
+        return node;
+    }
+}
+
+const preorder = [1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1];
+
+const obj = new Tree();
+const root = obj.add(preorder);
+
+console.log(root);
